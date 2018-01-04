9e8a98b99f43a2c6472220ed9814216bc5924ee1
ngiusti <nicholas.giusti89@gmail.com>
2018-01-03 21:59:25 -0800

    Add project files.

---
+++ PatatapClone/Scripts.js
@@ -0,0 +1,223 @@
+﻿
+
+var keyData = {
+	q: {
+		sound: new Howl({
+  		src: ['sounds/bubbles.mp3']
+		}),
+		color: '#1abc9c'
+	},
+	w: {
+		sound: new Howl({
+  		src: ['sounds/clay.mp3']
+		}),
+		color: '#2ecc71'
+	},
+	e: {
+		sound: new Howl({
+  		src: ['sounds/confetti.mp3']
+		}),
+		color: '#3498db'
+	},
+	r: {
+		sound: new Howl({
+  		src: ['sounds/corona.mp3']
+		}),
+		color: '#9b59b6'
+	},
+		t: {
+		sound: new Howl({
+  		src: ['sounds/dotted-spiral.mp3']
+		}),
+		color: '#34495e'
+	},
+	y: {
+		sound: new Howl({
+  		src: ['sounds/flash-1.mp3']
+		}),
+		color: '#16a085'
+	},
+	u: {
+		sound: new Howl({
+  		src: ['sounds/flash-2.mp3']
+		}),
+		color: '#27ae60'
+	},
+	i: {
+		sound: new Howl({
+  		src: ['sounds/flash-3.mp3']
+		}),
+		color: '#2980b9'
+	},
+	o: {
+		sound: new Howl({
+			src: ['sounds/glimmer.mp3']
+		}),
+		color: '#8e44ad'
+	},
+	p: {
+		sound: new Howl({
+  		src: ['sounds/moon.mp3']
+		}),
+		color: '#2c3e50'
+	},
+	a: {
+		sound: new Howl({
+  		src: ['sounds/pinwheel.mp3']
+		}),
+		color: '#f1c40f'
+	},
+	s: {
+		sound: new Howl({
+  		src: ['sounds/piston-1.mp3']
+		}),
+		color: '#e67e22'
+	},
+		d: {
+		sound: new Howl({
+  		src: ['sounds/piston-2.mp3']
+		}),
+		color: '#e74c3c'
+	},
+	f: {
+		sound: new Howl({
+  		src: ['sounds/prism-1.mp3']
+		}),
+		color: '#95a5a6'
+	},
+	g: {
+		sound: new Howl({
+  		src: ['sounds/prism-2.mp3']
+		}),
+		color: '#f39c12'
+	},
+	h: {
+		sound: new Howl({
+  		src: ['sounds/prism-3.mp3']
+		}),
+		color: '#d35400'
+	},
+	j: {
+		sound: new Howl({
+  		src: ['sounds/splits.mp3']
+		}),
+		color: '#1abc9c'
+	},
+	k: {
+		sound: new Howl({
+  		src: ['sounds/squiggle.mp3']
+		}),
+		color: '#2ecc71'
+	},
+	l: {
+		sound: new Howl({
+  		src: ['sounds/strike.mp3']
+		}),
+		color: '#3498db'
+	},
+	z: {
+		sound: new Howl({
+  		src: ['sounds/suspension.mp3']
+		}),
+		color: '#9b59b6'
+	},
+	x: {
+		sound: new Howl({
+  		src: ['sounds/timer.mp3']
+		}),
+		color: '#34495e'
+	},
+	c: {
+		sound: new Howl({
+  		src: ['sounds/ufo.mp3']
+		}),
+		color: '#16a085'
+	},
+	v: {
+		sound: new Howl({
+  		src: ['sounds/veil.mp3']
+		}),
+		color: '#27ae60'
+	},
+	b: {
+		sound: new Howl({
+  		src: ['sounds/wipe.mp3']
+		}),
+		color: '#2980b9'
+	},
+	n: {
+		sound: new Howl({
+			src: ['sounds/zig-zag.mp3']
+		}),
+		color: '#8e44ad'
+	},
+	m: {
+		sound: new Howl({
+  		src: ['sounds/moon.mp3']
+		}),
+		color: '#2c3e50'
+	}
+}
+
+var shapes = [];
+var count = 0
+
+function onKeyDown(event) {
+    if (keyData[event.key]) {
+        var maxPoint = new Point(view.size.width, view.size.height);
+        var randomPoint = Point.random();
+        var point = maxPoint * randomPoint;
+        shape(point);
+        clearing();
+    }   
+}
+
+function clearing() {
+    if (shapes.length >= 5) {
+        shapes.shift();
+    } 
+}
+
+function shape(point) {
+    if (count === 0) {
+        var triangle = new Path.RegularPolygon(point, 3, 10);
+        triangle.fillColor = keyData[event.key].color;
+        keyData[event.key].sound.play();
+        shapes.push(triangle);
+        count += 1;
+    }
+    else if (count === 1) {
+        var pentagon = new Path.RegularPolygon(point, 5, 10);
+        pentagon.fillColor = keyData[event.key].color;
+        keyData[event.key].sound.play();
+        shapes.push(pentagon);
+        count += 1;
+    } else if (count === 2) {
+        var hexagon = new Path.RegularPolygon(point, 6, 10);
+        hexagon.fillColor = keyData[event.key].color;
+        keyData[event.key].sound.play();
+        shapes.push(hexagon);
+        count += 1;
+    } else if (count === 3) {
+        var octagon = new Path.RegularPolygon(point, 8, 10);
+        octagon.fillColor = keyData[event.key].color;
+        keyData[event.key].sound.play();
+        shapes.push(octagon);
+        count += 1;
+    }  else {
+        var circle = new Path.Circle(point, 10);
+        circle.fillColor = keyData[event.key].color;
+        keyData[event.key].sound.play();
+        shapes.push(circle);
+        count = 0;
+    }
+}
+
+
+
+function onFrame(event) {
+    for (var i = 0; i < shapes.length; i++) {
+        shapes[i].scale(1.1);
+        shapes[i].rotate(3);
+    }
+}
